af38eea8d56f10f89efd21caf41a348fc10cf5f2
sulincix <parduscix@yandex.ru>
2024-12-22 20:09:10 +0000

    void linux

---
+++ base/void.sh
@@ -0,0 +1,32 @@
+set -e
+set -o pipefail
+
+tool_init(){
+    : dont required
+}
+install_base_system() {
+    cd /target
+    fname=$(wget -O - https://repo-default.voidlinux.org/live/current/ \
+        | grep x86_64 | grep ROOTFS | grep musl | head -n 1 | cut -f2 -d "\"")
+    wget https://repo-default.voidlinux.org/live/current/$fname -O - | xzcat | \
+    tar -xvf -
+    cat /etc/resolv.conf > /target/etc/resolv.conf
+    chroot /target xbps-pkgdb -ua
+    chroot /target/ xbps-install -Syu
+}
+
+install_package(){
+    chroot /target/ xbps-install -Sy $@
+}
+
+remove_package() {
+    chroot /target/ xbps-remove -Ry $@
+}
+
+update_initramfs() {
+    : dont required
+}
+
+configure() {
+    : fixme
+}
