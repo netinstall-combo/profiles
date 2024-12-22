34d5dbf21ab37ea240179aa5b77425519dd2ac6b
sulincix <parduscix@yandex.ru>
2024-12-22 21:08:41 +0000

    debian

---
+++ base/debian.sh
@@ -0,0 +1,35 @@
+set -e
+set -o pipefail
+source /netinstall/utils/iniparser.sh
+tool_init(){
+    apk add debootstrap
+}
+install_base_system() {
+    codename="$(ini_parse distro codename < /netinstall/data/profile)"
+    repo="$(ini_parse distro repository < /netinstall/data/profile)"
+    ln -s sid /usr/share/debootstrap/scripts/$codename || true
+    debootstrap $codename /target $repo
+    cat /etc/resolv.conf > /target/etc/resolv.conf
+}
+
+install_package(){
+    chroot /target/ apt install -yq $@
+}
+
+remove_package() {
+    chroot /target/ apt purge -yq $@
+}
+
+update_initramfs() {
+    : dont required
+}
+
+configure() {
+    case $1 in
+      testing)
+        echo "deb https://deb.debian.org/debian testing main contrib non-free non-free-firmware" > /target/etc/apt/sources.list
+        chroot /target apt update
+        chroot /target apt full-upgrade -yq
+        ;;
+    esac
+}
